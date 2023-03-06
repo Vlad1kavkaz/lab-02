6201c48a19baeeb6c4805d47531a9431ff904305
Vlad1kavkaz <162821820v@gmail.com>
2023-03-06 23:18:06 +0300

    this is first file

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+//
+// Created by vlad on 06.03.23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "hello world" << endl;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by vlad on 06.03.23.
+//
+
+#ifndef LB2_MAIN_H
+#define LB2_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //LB2_MAIN_H
