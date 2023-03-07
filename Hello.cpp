25524a1bec9f28111d53f434ca75fceb729ff8e9
Vlad1kavkaz <162821820v@gmail.com>
2023-03-07 18:31:43 +0300

    chenged style file

--- Hello.cpp
+++ Hello.cpp
@@ -5,9 +5,10 @@
 #include <iostream>
 #include <string>
 
-
-int main(int argc, char** argv) {
-    string name;                                         // Name of user
-    std::cin >> name;                                    // input name of user
-    std::cout << "hello world from" << name << std::endl;// output name of user
+int
+main(int argc, char** argv)
+{
+  string name;                                          // Name of user
+  std::cin >> name;                                     // input name of user
+  std::cout << "hello world from" << name << std::endl; // output name of user
 }
